aeb1125642b62fb034ea25f0ef8259fe495898f7
danIancuV <33359205+danIancuV@users.noreply.github.com>
2017-11-04 23:22:33 +0200

    remove unnecessary prop

--- MyRental/ViewModels/CarVM.cs
+++ MyRental/ViewModels/CarVM.cs
@@ -32,9 +32,6 @@ namespace MyRental.ViewModels
 
         public virtual ICollection<Reservation> Reservations { get; set; }
 
-        public string TestForGit { get; set; }
-		public string TestForGit2 { get; set; }
-
         public static CarVM MapTo(Car car)
         {
             return new CarVM
