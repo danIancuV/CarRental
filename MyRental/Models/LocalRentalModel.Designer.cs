48412c5ae5a80048f6a174fb9e4bdfa5b4fb5802
danIancuV <33359205+danIancuV@users.noreply.github.com>
2018-09-12 21:12:18 +0300

    local db configured app working locally last step before azure deploiement

--- MyRental/Models/LocalRentalModel.Designer.cs
+++ MyRental/Models/LocalRentalModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\user\Documents\GitHub\CarRental\MyRental\Models\LocalRentalModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\App\CarRental\MyRental\Models\LocalRentalModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
